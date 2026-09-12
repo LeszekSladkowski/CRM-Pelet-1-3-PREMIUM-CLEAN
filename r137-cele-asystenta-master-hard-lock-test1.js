fa16687d640603ec2b1774cd125425a3ac37b5ff
LESZEK ŚLADKOWSKI <tellechkielce@gmail.com>
2026-09-12 08:48:58 +0200

    PUNKT 1 KROK 1: chirurgiczne usunięcie nieaktywnych eksperymentów R132-R140
    Zabezpieczono Punkt 0 na osobnym branchu. Usunięto wyłącznie nieaktywne pliki testowe R132-R140 oraz notatkę R135. Dodano audyt SURGICAL_CLEANUP_POINT1_AUDIT.md. Aktywny runtime, MASTER-y, dane CRM, index.html, sw.js i version.json pozostają bez zmian.

--- r137-cele-asystenta-master-hard-lock-test1.js
+++
@@ -1,5 +0,0 @@
-/* R137 został wycofany jako warstwa końcowa po teście użytkownika.
-   Ten plik pozostaje wyłącznie jako bezpieczny most zgodności dla istniejącego importu z R136.
-   R138 przejmuje CAŁĄ końcową geometrię LIVE i wykonuje matematyczne centrowanie względem ramek rastra MASTER.
-*/
-importScripts('./r138-cele-asystenta-mathematical-centering-test1.js?v=R138-test1-cele-mathematical-centering-1914');
